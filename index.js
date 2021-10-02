30a12cbf4fcf16fdc1ccd28c787fa0ad6586852f
Ronak Patel <ronak0624@gmail.com>
2021-10-02 14:56:05 -0700

    front and back connected

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ const routes = require("./routes/search.js");
 
 app.use(express.static(path.join(__dirname, "views", "build")));
 
+app.use("/api", routes);
+
+
 app.use("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "build", "index.html"));
 })
 
-app.use("/api", routes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.use("/search", (req, res) => {
-    
+    res.status(200).json("Route working internally")
 });
 
 module.exports = router;
