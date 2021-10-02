6542124898277d9229c34bfb8a1e07ee22c46e14
Ronak Patel <ronak0624@gmail.com>
2021-10-02 14:26:21 -0700

    skeleton, express monorepo

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const routes = require("./routes/search.js");
+
+app.use(express.static(path.join(__dirname, "views", "build")));
+
+app.use("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "build", "index.html"));
+})
+
+app.use("/api", routes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ routes/search.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+router.use("/search", (req, res) => {
+    
+});
+
+module.exports = router;
