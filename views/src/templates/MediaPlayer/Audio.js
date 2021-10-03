219d5dce8ff830729339a5cd30c172d47946d1de
Ronak Patel <ronak0624@gmail.com>
2021-10-02 21:16:18 -0700

    skip, rewind, draggable progress bar for audio

--- views/src/templates/MediaPlayer/Audio.js
+++ views/src/templates/MediaPlayer/Audio.js
@@ -33,8 +33,7 @@ export default function Audio() {
             audio.removeEventListener("loadeddata", setAudioData);
             audio.removeEventListener("timeupdate", setAudioTime);
         }
-    }
-    );
+    });
 
     return {
         currentTime,
--- views/src/templates/MediaPlayer/index.js
+++ views/src/templates/MediaPlayer/index.js
@@ -1,14 +1,16 @@
-import ReactAudioPlayer from 'react-audio-player';
 import audioPlayer from './Audio';
-import Play from "./Play"
-import Pause from "./Pause"
+import Play from "./Play";
+import Pause from "./Pause";
+import Rewind from './Rewind';
+import Skip from './Skip';
+import Progress from './Progress';
 
 export default function Player(props) {
     const { src, type, song } = props;
     let { currentTime, duration, playing, setPlaying, setClickedTime } = false;
 
 
-    if (src) {
+    if (type === 'audio') {
         ({ currentTime, duration, playing, setPlaying, setClickedTime } = audioPlayer());
     }
 
@@ -16,12 +18,19 @@ export default function Player(props) {
         <div className="w-screen fixed bottom-0 bg-gray-700 p-5 text-gray-50">
             <p>{src}</p>
             {type === "audio" ?
-                <div className="w-full flex justify-center h-10">
-                    <audio id="audio" autoPlay src={src}></audio>
-                    {playing ?
-                        <Pause handleClick={() => setPlaying(false)} /> :
-                        <Play handleClick={() => setPlaying(true)} />
-                    }
+                <div className="w-full">
+
+                    <Progress currentTime={currentTime} duration={duration} onTimeUpdate={(time) => setClickedTime(time)} />
+                    <div className="w-full h-10 flex justify-center">
+                        <audio id="audio" autoPlay src={src}></audio>
+                        <Rewind handleClick={() => setClickedTime(currentTime - 5)} />
+                        {playing ?
+                            <Pause handleClick={() => setPlaying(false)} /> :
+                            <Play handleClick={() => setPlaying(true)} />
+                        }
+
+                        <Skip handleClick={() => setClickedTime(currentTime + 5)} />
+                    </div>
                 </div>
                 :
                 <video src={src} autoPlay controls />
